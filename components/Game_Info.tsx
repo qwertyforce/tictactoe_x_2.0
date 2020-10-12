4a748d9d153ab1d9305ec45d620ceebacba15c7d
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-10-12 21:40:01 +0300

    Update Game_Info.tsx

--- components/Game_Info.tsx
+++ components/Game_Info.tsx
@@ -1,11 +1,17 @@
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
+import ListGroup from 'react-bootstrap/ListGroup'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faUserAlt} from '@fortawesome/free-solid-svg-icons'
 import styles from "../styles/GameInfo.module.css"
+import { useState } from 'react'
 export default function nav_bar(props) {
+    const [selectedItem, setSelectedItem] = useState("");
     console.log(props)
+    const alertClicked=(e)=>{
+        setSelectedItem((selectedItem===e.target.id?"":e.target.id))
+    }
     // const profile_and_stats = () => {
     //     if (props.authed) {
     //         return (
@@ -35,6 +41,16 @@ export default function nav_bar(props) {
 </Card>
 <Card>
     <Card.Header>{`Your Figure: ${props.figure||'X'}`}</Card.Header>
+</Card>
+<Card>
+    <Card.Header>Your Bonuses</Card.Header>
+    <ListGroup >
+    <ListGroup.Item action id="set_block" variant={(selectedItem==="set_block")?("success"):""} onClick={alertClicked}>set_block {`(x${props.set_block || 1})`}</ListGroup.Item>
+    <ListGroup.Item action id="destroy_block" variant={(selectedItem==="destroy_block")?("success"):""}  onClick={alertClicked}>destroy_block {`(x${props.destroy_block || 1})`}</ListGroup.Item>
+    <ListGroup.Item action id="destroy_player_figure" variant={(selectedItem==="destroy_player_figure")?("success"):""}  onClick={alertClicked}>destroy_player_figure {`(x${props.destroy_player_figure || 1})`}</ListGroup.Item>
+    <ListGroup.Item action id="enemy_figure_transform" variant={(selectedItem==="enemy_figure_transform")?("success"):""}  onClick={alertClicked} >enemy_figure_transform {`(x${props.enemy_figure_transform || 1})`}</ListGroup.Item>
+    <ListGroup.Item action id="mine"variant={(selectedItem==="mine")?("success"):""}  onClick={alertClicked}>mine {`(x${props.mine || 1})`}</ListGroup.Item>
+    </ListGroup >
 </Card>
 <Card>
     <Card.Header>
