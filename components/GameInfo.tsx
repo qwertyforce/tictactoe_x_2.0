e86ef80421fd17ca46a6968605185c05ca2e64a1
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-10-18 22:22:40 +0300

    GameInfo

--- components/GameInfo.tsx
+++ components/GameInfo.tsx
@@ -5,43 +5,32 @@ import ListGroup from 'react-bootstrap/ListGroup'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserAlt } from '@fortawesome/free-solid-svg-icons'
 import styles from "../styles/GameInfo.module.css"
+import GameDataContext from './GameDataContext'
 import { useState } from 'react'
 
 export default function GameInfo(props) {
+    let GameData=props.gameData
+    const usernames=GameData.players.map((username:string)=><p className={styles.username}><FontAwesomeIcon icon={faUserAlt} /> {username}</p>);
     const [selectedItem, setSelectedItem] = useState("");
     console.log(props)
     const alertClicked = (e) => {
         setSelectedItem((selectedItem === e.target.id ? "" : e.target.id))
     }
-    // const profile_and_stats = () => {
-    //     if (props.authed) {
-    //         return (
-    //             <Fragment>
-    //                 <Nav.Link href="/profile">Profile</Nav.Link>
-    //                 <Nav.Link href="/logout">Log out</Nav.Link>
-    //             </Fragment>
-    //         )
-    //     } else {
-    //         null
-    //     }
-    // }
     return (
-        <Col md={2} style={{ padding: 0 }}>
+        <Col md={2} style={{ padding: 0 }} ref={props.gameData.GameInfoRef}>
             <Card>
-                <Card.Header>{`Time for move: ${5}s`}</Card.Header>
+                <Card.Header>{`Time for move: ${GameData.time}s`}</Card.Header>
             </Card>
             <Card>
                 <Card.Header>{`Players (${props.current_player_count || 1} of ${props.max_player_count || 2})`}</Card.Header>
                 <Card.Body style={{ padding: 0 }}>
-                    <p className={styles.username}><FontAwesomeIcon icon={faUserAlt} /> wf</p>
-                    <p className={styles.username}><FontAwesomeIcon icon={faUserAlt} /> wf</p>
-                    <p className={styles.username}><FontAwesomeIcon icon={faUserAlt} /> wf</p>
-                    <p className={styles.username}><FontAwesomeIcon icon={faUserAlt} /> wf</p>
+                    {usernames}
                 </Card.Body>
             </Card>
             <Card>
                 <Card.Header>{`Your Figure: ${props.figure || 'X'}`}</Card.Header>
             </Card>
+            {GameData.offline?(null):(
             <Card>
                 <Card.Header>Your Bonuses</Card.Header>
                 <ListGroup >
@@ -51,7 +40,9 @@ export default function GameInfo(props) {
                     <ListGroup.Item action id="enemy_figure_transform" variant={(selectedItem === "enemy_figure_transform") ? ("success") : ""} onClick={alertClicked} >enemy_figure_transform {`(x${props.enemy_figure_transform || 1})`}</ListGroup.Item>
                     <ListGroup.Item action id="mine" variant={(selectedItem === "mine") ? ("success") : ""} onClick={alertClicked}>mine {`(x${props.mine || 1})`}</ListGroup.Item>
                 </ListGroup >
-            </Card>
+            </Card>)
+            }
+            
             <Card>
                 <Card.Header>
                     <Form.Check style={{ display: 'flex' }} inline label="Play sound when it is  your turn to play" type="checkbox" />
