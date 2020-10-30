12f2e246c1c600dae323e48189eb68f5430d199d
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-10-30 14:33:56 +0300

    new win checking algorithm

--- components/Game.tsx
+++ components/Game.tsx
@@ -8,12 +8,6 @@ import styles from "../styles/GameInfo.module.css"
 import { useEffect, useState,useRef } from 'react'
 import GameOverModal from './GameOverModal'
 
-function randomInteger(min, max) {
-    let rand = min + Math.random() * (max + 1 - min);
-    rand = Math.floor(rand);
-    return rand;
-}
-
 function divide(numerator, denominator) {
     const remainder = numerator % denominator;
     const quotient = (numerator - remainder) / denominator;
@@ -34,7 +28,7 @@ function game(canvas,setMargin,setGameData,game_over){
         console.log(`StateCacheHits: ${e.data.StateCacheHits}`)
         console.log(`StateCachePuts: ${e.data.StateCachePuts}`)
         console.log(e.data.firstMoves)
-        make_move(e.data.bestmove.j, e.data.bestmove.i,1)
+        make_move(e.data.bestmove.i, e.data.bestmove.j,1)
     }
     const Game_Board = new Array(21).fill(null).map(() => new Array(21).fill(0))
     const Time_for_move = parseInt(prompt("Enter the time for ai to think (in seconds)")||"0")*1000
@@ -110,8 +104,8 @@ function game(canvas,setMargin,setGameData,game_over){
             const xx = divide(xz, g_cellSize);
             const yy = divide(yz, g_cellSize);
                 if (Game_Board[yy][xx] === 0) {
-                   make_move(xx,yy,gameData.your_player_idx)
-                   console.log(xx, yy);
+                   make_move(yy,xx,gameData.your_player_idx)
+                   console.log(yy, yy);
                 }
         }  
     }, false);
@@ -184,28 +178,109 @@ function game(canvas,setMargin,setGameData,game_over){
          })
          timer = setInterval(timer_Func, 1000);
      }
+     
+     function get_vector(i){
+        switch(i){
+            case 0:
+                return [1,0]
+            case 1:
+                return [0,1]
+            case 2:
+                return [1,1]
+            case 3:
+                return [-1,1]
+        }
+    }
+    function check_directions(arr,i) {
+        const vector=get_vector(i)
+        const reference_point=arr[arr.length-1]
+        for (let i = 0; i < arr.length - (4+1); i++) {
+            if (arr[i] !== 0) {
+                if (arr[i] === arr[i + 1] && arr[i] === arr[i + 2] && arr[i] === arr[i + 3] && arr[i] === arr[i + 4]) {
+                    const win_xs=[]
+                    const win_ys=[]
+                    for(let k=i;k<=i+4;k++){
+                        win_xs.push(reference_point[0]+k*vector[0])
+                        win_ys.push(reference_point[1]+k*vector[1])
+                    }
+                    return [win_xs,win_ys]
+                }
+            }
+    
+        }
+        return false
+    }
 
+     function get_directions(Board, x, y) {
+        const Directions = [[],[],[],[]];
+        let dir0=-1
+        let dir1=-1
+        let dir2=-1
+        let dir3=-1
+        for (let i = -4; i < 5; i++) {
+            if (x + i >= 0 && x + i <= Rows - 1) {
+                if(dir0===-1){dir0=[x+i,y]}
+                Directions[0].push(Board[x + i][y])
+                if (y + i >= 0 && y + i <= Columns - 1) {
+                    if(dir2===-1){dir2=[x+i,y+i]}
+                    Directions[2].push(Board[x + i][y + i])
+                }
+            }
+            if (y + i >= 0 && y + i <= Columns - 1) {
+                if(dir1===-1){dir1=[x,y+i]}
+                Directions[1].push(Board[x][y + i])
+                if (x - i >= 0 && x - i <= Rows - 1) {
+                    if(dir3===-1){dir3=[x-i,y+i]}
+                    Directions[3].push(Board[x - i][y + i])
+                }
+            }
+        }
+        Directions[0].push(dir0)
+        Directions[1].push(dir1)
+        Directions[2].push(dir2)
+        Directions[3].push(dir3)
+        return Directions
+    }
+
+     function checkwin(Board, x, y,player_idx) {
+        const Directions = get_directions(Board, x, y)
+        console.log(Directions)
+        for (let i = 0; i < 4; i++) {
+            const res=check_directions(Directions[i],i)
+            if (res) {
+                const xs=res[0]
+                const ys=res[1]
+                draw_winning_line(xs,ys,player_idx);
+                return true
+            }
+        }
+    }
+
+    
     function make_move(column, row, player_idx) {
         if ((prev_move_column !== null) && (prev_move_column !== undefined)) {
             ctx.fillStyle = colorOfplayer(prev_player_idx);
-            drawBox(prev_move_column, prev_move_row);
+            drawBox(prev_move_row, prev_move_column);
         }
         moves_made++
         prev_move_column = column;
         prev_move_row = row;
         prev_player_idx = player_idx;
         console.log(Game_Board)
-        Game_Board[row][column] = (player_idx===0)?-1:1;
+        Game_Board[column][row] = (player_idx === 0) ? -1 : 1;
         const set_figure = figureOfplayer(player_idx);
-        set_figure(column, row);
-        if (check_win(player_idx)) {
+        set_figure(row,column);
+        console.log(prev_move_column,prev_move_row)
+        if (checkwin(Game_Board,prev_move_column,prev_move_row,player_idx)) {
+            Paused = true;
+            game_over((player_idx===gameData.your_player_idx)?"won":"lost")
             clearInterval(timer);
-        }else{
-            if(moves_made===21*21){
+        } else {
+            if (moves_made === 21 * 21) {
                 Paused = true;
                 clearInterval(timer);
                 game_over("draw")
-            }else{
+            } else {
                 Move_transition();
                 if (prev_player_idx === gameData.your_player_idx) {
                     Engine.postMessage([Game_Board, 1, Time_for_move]); //time in ms
@@ -289,62 +364,19 @@ function game(canvas,setMargin,setGameData,game_over){
         ctx.closePath();
         ctx.stroke();
     };
-    function drawWinLine(xs, ys, r, g, b, player_idx) {
-        const Winnerfigure = figureOfplayer(player_idx);
-        for (let i = 0; i < (xs.length); i += 1) {
-            clear_cell(xs[i], ys[i]);
-            Winnerfigure(xs[i], ys[i]);
-            ctx.fillStyle = "rgba(" + r + "," + g + "," + b + "," + 0.5 + ")";
-            drawBox(xs[i], ys[i]);
-        }
-    };
+
     function drawBox (x, y) {
         ctx.fillRect(x * g_cellSize + offset + lineWidth/2, y * g_cellSize + offset + lineWidth/2, g_cellSize - lineWidth, g_cellSize - lineWidth);
     };
-    function check_win(player_idx) {
-        let r, g, h, won, xx, yy, xs, ys;
-        for (let i = 0; i < Game_Board.length; i++) {
-            for (let j = 0; j < Game_Board[i].length; j++) {
-                for (let d = 0; d < 360; d = d + 45) {
-                    r = d * Math.PI / 180;
-                    g = Math.round(Math.sin(r));
-                    h = Math.round(Math.cos(r));
-                    won = true;
-                    xx = i;
-                    yy = j;
-                    xs = new Array;
-                    ys = new Array;
-                    for (let ii = 0; ii <= (FiguresToWin - 1); ii++) {
-                        if (xx < 0 || xx >= Game_Board.length || yy < 0 || yy >= Game_Board[0].length) {
-                            won = false;
-                            break;
-                        }
-                        const piece = (player_idx===0)?(-1):(1)
-                        if (Game_Board[xx][yy] !== piece) {
-                            won = false;
-                            break;
-                        }
-                        xs.push(xx);
-                        ys.push(yy);
-                        xx += g;
-                        yy += h;
-                    }
-                    if (won == true) {
-                        console.log("won");
-                        console.log(xx);
-                        console.log(yy);
-                        draw_won(xs,ys,player_idx);
-                        return true;
-                    }
-                }
-            }
-        }
-    };
     
-    function draw_won(xs, ys, player_idx) {
-        Paused = true;
-        drawWinLine(ys, xs, 255, 20, 147, player_idx);
-        game_over((player_idx===gameData.your_player_idx)?"won":"lost")
+    function draw_winning_line(xs, ys, player_idx) {
+        const set_figure = figureOfplayer(player_idx);
+        for (let i = 0; i < (xs.length); i += 1) {
+            clear_cell(xs[i], ys[i]);
+            set_figure(xs[i], ys[i]);
+            ctx.fillStyle = "rgba(255,20,147,0.5)"
+            drawBox(xs[i], ys[i]);
+        }
     }
 
     function clear_cell(x, y) {
