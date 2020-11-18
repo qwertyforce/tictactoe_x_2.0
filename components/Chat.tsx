803d04792dc677debdd19e28f6192fb84f24ef36
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-11-18 14:19:06 +0300

    fix typescript errors

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,18 +1,10 @@
 import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-import Form from 'react-bootstrap/Form'
-import ListGroup from 'react-bootstrap/ListGroup'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserAlt } from '@fortawesome/free-solid-svg-icons'
-import { useEffect, useState,useRef } from 'react'
-import GameOverModal from './GameOverModal'
-import io from 'socket.io-client';
-import { useRouter } from 'next/router'
+import { useState } from 'react'
 import styles from "../styles/Chat.module.css"
 
-export default function Chat(props) {
+export default function Chat(props:any) {
     const [text,setText] = useState("");
-    const handleChange=(e)=>{
+    const handleChange=(e:any)=>{
         setText(e.target.value)
     }
     const send_msg=()=>{
