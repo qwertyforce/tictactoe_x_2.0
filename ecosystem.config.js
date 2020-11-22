8618d6dd6e3e678ec4aacef9652962d1137298b8
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-11-22 18:14:25 +0300

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: '4battle',
+  script: 'npm',
+  args: 'start',
+  log_file: 'log.txt',
+  time: true
+};
---
+++ ws.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: '4battle_ws',
+  script: 'npm',
+  args: 'start_ws',
+  log_file: 'log_ws.txt',
+  time: true
+};
